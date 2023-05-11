e12a62901f3f02f00c8d7e0f6046318f80046dc0
Ivan Jijon <ivanjijon@autonomia.digital>
2023-05-10 23:34:45 -0500

    added missing ;

--- orbits_with_restrictions/sketch.js
+++ orbits_with_restrictions/sketch.js
@@ -6,16 +6,16 @@ function setup() {
 
 function draw() {
     translate(width / 2, height / 2);
-    background(255)
+    background(255);
     colorMode(RGB);
 
     const POSITIVE = -1;
     const NEGATIVE = 1;
 
-    displayOrbit(color(155, 210, 163), createOrbit(frameCount, 100, 10, NEGATIVE))
-    displayOrbit(color(61, 69, 99), createOrbit(frameCount * 2, 150, 30, POSITIVE))
-    displayOrbit(color(191, 105, 122), createOrbit(frameCount * 2, 200, 20, NEGATIVE))
-    displayOrbit(color(240, 184, 127), createOrbit(frameCount, 250, 10, POSITIVE))
+    displayOrbit(color(155, 210, 163), createOrbit(frameCount, 100, 10, NEGATIVE));
+    displayOrbit(color(61, 69, 99), createOrbit(frameCount * 2, 150, 30, POSITIVE));
+    displayOrbit(color(191, 105, 122), createOrbit(frameCount * 2, 200, 20, NEGATIVE));
+    displayOrbit(color(240, 184, 127), createOrbit(frameCount, 250, 10, POSITIVE));
 }
 
 function createOrbit(angle, radius, size, direction) {
@@ -40,7 +40,7 @@ function displayOrbit(fillColor, orbit) {
 
 function keyPressed() {
     if (key === 's') {
-        const seconds = 10
+        const seconds = 10;
         saveGif('orbits.gif', seconds);
     }
 }
